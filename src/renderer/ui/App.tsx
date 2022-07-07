16a15da63b2d083b805da1bfe135469634f2b460
Vinícius Henrique do Nascimento <vinicius_henrique889@hotmail.com>
2022-07-07 20:20:07 -0300

    Começando

--- src/renderer/ui/App.tsx
+++ src/renderer/ui/App.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react"
+
 import { getDate } from "../../common/getDate"
 
 import styles from "./App.module.scss"
@@ -6,11 +7,10 @@ import logo from "../public/logo192.png"
 
 export const App: React.FC = () => {
 
-  const [date, setDate] = useState(getDate())
-
-  useEffect(() => {
-    setTimeout(() => setDate(getDate()), 1000)
-  }, [date, setDate])
+  const [infoItem, setinfoItem] = useState<any>([])
+  const [iId, setitemId] = useState<string>(``)
+  
+  
 
   return (
     <div className={styles.app}>
@@ -18,9 +18,20 @@ export const App: React.FC = () => {
         src={logo}
         alt="React logo"
       />
-      <h1>Electron React Typescript</h1>
-      <p>Code changes will reload the app automatically.</p>
-      <pre>{date}</pre>
+
+      <h1>Sistema de Cadastro</h1>
+      <h5>Opções da aplicação:</h5>
+
+      <div className="cad">
+        <p>Realizar um cadasto:</p>
+        <button>Cadastrar</button>
+      </div>
+      
+      <div className="listCad">
+        <p>Lista de Cadastros:</p>
+        <button>Listar</button>
+      </div>
+      
     </div>
   )
 }
